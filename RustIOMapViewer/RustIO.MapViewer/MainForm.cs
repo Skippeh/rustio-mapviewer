212c9d4350c209d0a8c89229f7fd57477d807f74
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-03-26 19:59:48 +0200

    Removed WPF project, replacing with win forms and CefSharp

---
+++ RustIOMapViewer/RustIO.MapViewer/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace RustIO.MapViewer
+{
+    public partial class MainForm : Form
+    {
+        private ChromiumWebBrowser browser;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Cef.Initialize();
+
+            browser = new ChromiumWebBrowser("http://www.google.com");
+            browser.Dock = DockStyle.Fill;
+
+            Controls.Add(browser);
+        }
+    }
+}
